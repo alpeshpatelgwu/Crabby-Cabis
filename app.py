4f96f504299e0435d1d76e635362cdae8cf6c89b
Alpesh Patel <alpesh.patel.army@gmail.com>
2019-04-11 21:29:39 -0400

    stable SQL Query / Empty Station

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ pymysql.install_as_MySQLdb()
 
 import pandas as pd
 
+from datetime import date
+
 from flask import (
     Flask,
+    request,
     render_template,
     jsonify)
 
 from flask_sqlalchemy import SQLAlchemy
+
+from flask.json import JSONEncoder
 
 remote_db_endpoint = 'gwcodingbootcamp.cr0gccbv4ylw.us-east-2.rds.amazonaws.com'
 remote_db_port = '3306'
@@ -26,7 +31,22 @@ print(engine)
 # Create a remote database engine connection
 conn = engine.connect()
 
+class CustomJSONEncoder(JSONEncoder):
+
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+
 app = Flask(__name__)
+
+app.json_encoder = CustomJSONEncoder
 
 # Create a route that renders index.html template
 @app.route("/")
@@ -42,16 +62,66 @@ def station_status():
 @app.route("/data")
 def data():
     
-    
+
+    # start_date = request.args.get("startDate")
+    # end_date = request.args.get("endDate")
+
 
     conn = engine.connect()
 
-    # bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time BETWEEN '2019-04-06 09:52:00' AND '2019-04-07 09:52:00'", conn)
+    #bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time BETWEEN '2019-04-06 09:52:00' AND '2019-04-07 09:52:00'", conn)
 
-    bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time = '2019-04-06 09:52:00'", conn)
+    #bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time = '%{start_date} 09:52:00'", conn, 
+    #                        params={"start_date":start_date})
+
+#SELECT * FROM bikeshare_db.bikeshare WHERE time LIKE '2019-04-06%' AND num_bikes = 0 GROUP BY term_id;
+    bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time ='2019-04-06 09:52:00'", conn)
+
+
     #return jsonify(bike_df.to_dict())
-    bike_json = bike_df.to_json(orient="records")
-    return bike_json
+    return jsonify(bike_df.to_dict(orient="records"))
+
+
+
+@app.route("/jisan")
+def jisan():
+    
+    conn = engine.connect()
+
+    #print(request.args.get("name"))
+    #print("fuck this")
+
+    start_date = request.args.get("startDate")
+    # end_date = request.args.get("endDate")
+
+
+    bike_df = pd.read_sql(f"SELECT * FROM bikeshare WHERE time LIKE '{start_date}%%' AND num_bikes = 0 GROUP BY term_id", conn, 
+                            params={"start_date":start_date})
+
+    #bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time BETWEEN '2019-04-06 09:52:00' AND '2019-04-07 09:52:00'", conn)
+
+#    bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time = '2019-04-06 09:52:00'", conn, 
+#                            params={"start_date":start_date}) 
+
+
+    #bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time LIKE `2019-04-06 %`", conn)
+#                            params={"start_date":start_date})
+
+    #SELECT * FROM bikeshare_db.bikeshare WHERE time LIKE '2019-04-06%' AND num_bikes = 0 GROUP BY term_id;
+    #bike_df = pd.read_sql("SELECT * FROM bikeshare WHERE time ='2019-04-06 09:52:00'", conn)  
+
+    #%{start_date}
+    #return jsonify(bike_df.to_dict())
+    # bike_json = bike_df.to_json(orient="records")
+    
+    return jsonify(bike_df.to_dict(orient="records"))
+
+    #return(start_date)
+
+@app.route("/tom")
+def tom():
+    print(request.args.get("name"))
+    return("You are here Tom")
 
 if __name__ == "__main__":
     app.run(debug=True)
